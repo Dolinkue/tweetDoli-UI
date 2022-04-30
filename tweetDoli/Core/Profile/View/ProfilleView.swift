dbcf28f1df6c259a25d6b3618db542bd8a380a43
dolinkue <dolinkue_n@me.com>
2022-04-30 16:47:28 -0300

    view

--- tweetDoli/Core/Profile/View/ProfilleView.swift
+++ tweetDoli/Core/Profile/View/ProfilleView.swift
@@ -9,31 +9,77 @@ import SwiftUI
 
 struct ProfilleView: View {
     var body: some View {
-        VStack{
+        VStack (alignment: .leading){
            
             headerView
+        
+            actionbuttons
             
-            HStack {
-                Spacer()
-                
-                Image(systemName: "bell.badge")
-                    .font(.title3)
-                    .padding(6)
-                    .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
-                
-                Button {
-                    
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline).bold()
-                        .frame(width: 120, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray,lineWidth: 0.75))
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    Text("Nico dolinkue")
+                        .font(.title2).bold()
+                    Image(systemName: "checkmark.seal.fill")
+                        .foregroundColor(Color(.systemBlue))
                 }
-
+                
+                Text("@nicoDolinkue")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                
+                Text("alto gato")
+                    .font(.subheadline)
+                    .padding(.vertical)
+                
+                HStack(spacing: 24){
+                    
+                    HStack {
+                        Image(systemName: "mappin.and.ellipse")
+                        Text("carmen de areco")
+                        
+                    }
+                    
+                    
+                    
+                    HStack{
+                        Image(systemName: "link")
+                        Text("github/Dolinkue")
+                        
+                    }
+                }
+                .font(.caption)
+                .foregroundColor(.gray)
+                
+                HStack(spacing: 24){
+                    
+                    HStack(spacing: 4) {
+                        Text("200")
+                            .font(.subheadline)
+                            .bold()
+                        Text("Following")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                    
+                    
+                    
+                    HStack(spacing: 4) {
+                        Text("1M")
+                            .font(.subheadline)
+                            .bold()
+                        Text("Followers")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                }
+                
+                padding(.vertical)
+                
+                
             }
-            
-            
+            .padding(.horizontal)
             
             Spacer()
             
@@ -76,6 +122,28 @@ extension ProfilleView {
         
     }
     
-    
+    var actionbuttons: some View {
+        
+        HStack(spacing: 12) {
+            Spacer()
+            
+            Image(systemName: "bell.badge")
+                .font(.title3)
+                .padding(6)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
+            
+            Button {
+                
+            } label: {
+                Text("Edit Profile")
+                    .font(.subheadline).bold()
+                    .frame(width: 120, height: 32)
+                    .foregroundColor(.black)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray,lineWidth: 0.75))
+            }
+
+        }
+        .padding(.trailing)
+    }
     
 }
