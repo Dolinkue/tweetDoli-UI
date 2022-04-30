756d85f4bdf8044c8addb22ccc612e40f6b70db1
dolinkue <dolinkue_n@me.com>
2022-04-29 21:12:53 -0300

    Initial Commit

---
+++ tweetDoli/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tweetDoli
+//
+//  Created by Nicolas Dolinkue on 29/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tweetDoli/tweetDoliApp.swift
@@ -0,0 +1,17 @@
+//
+//  tweetDoliApp.swift
+//  tweetDoli
+//
+//  Created by Nicolas Dolinkue on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct tweetDoliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
